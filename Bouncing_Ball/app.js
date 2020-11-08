e86676654f6bc7a25c7f9c984a7c8a92e3806b63
parkjoungwan <whddhs6645@gmail.com>
2020-11-08 23:15:47 +0900

    test

--- Bouncing_Ball/app.js
+++ Bouncing_Ball/app.js
@@ -7,7 +7,7 @@ class App {
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext('2d');
 
-        document.body.appendChile(this.canvas);
+        document.body.appendChild(this.canvas);
 
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize();
