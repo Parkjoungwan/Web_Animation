4ba5eb7a86dd3af39a6ba58115347646c6f62b71
parkjoungwan <whddhs6645@gmail.com>
2020-11-09 16:22:24 +0900

    10/09

--- Bouncing_Ball/app.js
+++ Bouncing_Ball/app.js
@@ -2,6 +2,10 @@ import {
     Ball
 } from './ball.js';
 
+import {
+	Block
+} from './block.js';
+
 class App {
     constructor() {
         this.canvas = document.createElement('canvas');
@@ -12,7 +16,8 @@ class App {
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize();
 
-        this.ball = new Ball(this.stageWidth, this.stageHeight, 60, 15);
+	this.ball = new Ball(this.stageWidth, this.stageHeight, 30, 15);
+	this.block = new Block(700, 30, 300, 450);	
 
         window.requestAnimationFrame(this.animate.bind(this));
     }
@@ -28,8 +33,11 @@ class App {
 
     animate(t) {
         window.requestAnimationFrame(this.animate.bind(this));
+	
+	this.ctx.clearRect(0, 0, this.stageWidth, this.stageHeight);
 
-        this.ball.draw(this.ctx, this.stageWidth, this.stageHeight);
+    	this.block.draw(this.ctx);
+	this.ball.draw(this.ctx, this.stageWidth, this.stageHeight, this.block);
     }
 }
 
